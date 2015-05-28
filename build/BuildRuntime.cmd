ea122bca8b9916a596c724de4349b3788394b222
nate-untiedt <unti0008@gmail.com>
2015-05-27 23:23:48 -0500

    Improved build cmd script
    Made script independent of invocation location; Cleaned up console
    output; eliminated need to manually update file list when source *.qvs
    files are added or deleted

--- build/BuildRuntime.cmd
+++ build/BuildRuntime.cmd
@@ -1,39 +1,26 @@
+@ECHO OFF
+@Title Qlikview Components Build Process
 REM ***************************************************************************************************************
 REM This cmd file is for people who don't want to use ANT (requires Java & ANT) to rebuild the Runtime\Qvc.qvs >>..\Qvc_Runtime\Qvc.qvs file. 
-REM Currently, the list of files must be maintained manually. 
 REM ****************************************************************************************************************
-REM ** Clean out the Qvc_Runtime directory
-DEL /S /Q ..\Qvc_Runtime\*.*
+CD "%~dp0"
+ECHO Cleaning Qvc_Runtime directory...
+DEL /S /Q ..\Qvc_Runtime\*.* > nul
 
-REM ** Merge source *.qvs files into a single Qvc.qvs file.
-REM ** The reason a wildcard copy can't be used is because TYPE ..\QVC_Source\Qvc_Global.qvs must be first. The remaining files can be copied in any order.
+ECHO Merging source *.qvs files into Qvc.qvs...
+
+REM ..\QVC_Source\Qvc_Global.qvs must be first.
 TYPE ..\QVC_Source\Qvc_Global.qvs > ..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_AsOfTable.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_Calendar.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_Cleanup.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_ColorTheme.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_DataLineage.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_DbExtract.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_EmptyQvd.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_ExpandInterval.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_Incremental_Reload.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_InspectTable.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_Icons.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_JoinGenericTables.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_LinkTable.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_ListDirectories.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_ListFiles.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_Log.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_PopulateVariables.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_QvcAvailableUpdate.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_SegmentedStore.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_TableStats.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_Utility.qvs >>..\Qvc_Runtime\Qvc.qvs
-TYPE ..\QVC_Source\Qvc_WildMap.qvs >>..\Qvc_Runtime\Qvc.qvs
+FOR %%b IN (..\QVC_Source\*.qvs) DO (
+    IF NOT "%%b"=="..\QVC_Source\Qvc_Global.qvs" (
+        TYPE %%b >> ..\Qvc_Runtime\Qvc.qvs
+    )
+)
 
-XCOPY ..\QVC_Source\Language\*.* ..\Qvc_Runtime\Language\*.* /E
+ECHO Copying language files...
+XCOPY ..\QVC_Source\Language\*.* ..\Qvc_Runtime\Language\*.* /E > nul
 
-@ECHO OFF
-set /p=Hit ENTER to Finish
+ECHO Qlikview Components build complete!
+
+SET /p=Press ENTER to finish...
 @ECHO ON
-
