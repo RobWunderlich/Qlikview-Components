1abb49fe200396ae3047420dc96f276a86a7f17b
nate-untiedt <unti0008@gmail.com>
2015-06-01 12:14:18 -0500

    Refactored BuildRuntime.cmd
    * Added documentation reload task
    * Organized steps into separate functions
    * Added variables for commonly used directory paths

--- build/BuildRuntime.cmd
+++ build/BuildRuntime.cmd
@@ -3,25 +3,52 @@
 REM ***************************************************************************************************************
 REM This cmd file is for people who don't want to use ANT (requires Java & ANT) to rebuild the Runtime\Qvc.qvs >>..\Qvc_Runtime\Qvc.qvs file. 
 REM ****************************************************************************************************************
-REM ** Make sure we are in the build directory so we can call this script from anywhere
-CD "%~dp0"
-ECHO Cleaning Qvc_Runtime directory...
-DEL /S /Q ..\Qvc_Runtime\*.* > nul
 
-ECHO Merging source *.qvs files into Qvc.qvs...
-
-REM ..\QVC_Source\Qvc_Global.qvs must be first.
-TYPE ..\QVC_Source\Qvc_Global.qvs > ..\Qvc_Runtime\Qvc.qvs
-FOR %%b IN (..\QVC_Source\*.qvs) DO (
-    IF NOT "%%b"=="..\QVC_Source\Qvc_Global.qvs" (
-        TYPE %%b >> ..\Qvc_Runtime\Qvc.qvs
-    )
-)
-
-ECHO Copying language files...
-XCOPY ..\QVC_Source\Language\*.* ..\Qvc_Runtime\Language\*.* /E > nul
+call :setup
+call :clean
+call :build
+call :doc
 
 ECHO Qlikview Components build complete!
 
 SET /p=Press ENTER to finish...
+GOTO :End
+
+:setup
+    REM ** Make sure we are in the build directory so we can call this script from anywhere
+    CD "%~dp0"
+    REM Setup configuration for running qlikview
+    SET qveDir="C:\Program Files\QlikView"
+    SET qveExec="qv.exe"
+    SET runtimeDir="..\Qvc_Runtime"
+    SET srcDir="..\Qvc_Source"
+    SET docDir="..\Doc"
+GOTO:EOF
+
+:build
+    ECHO Merging source *.qvs files into Qvc.qvs...
+
+    REM ..\QVC_Source\Qvc_Global.qvs must be first.
+    TYPE %srcDir%\Qvc_Global.qvs > %runtimeDir%\Qvc.qvs
+    FOR %%b IN (%srcDir%\*.qvs) DO (
+        IF NOT "%%b"=="%srcDir%\Qvc_Global.qvs" (
+            TYPE %%b >> %runtimeDir%\Qvc.qvs
+        )
+    )
+
+    ECHO Copying language files...
+    XCOPY %srcDir%\Language\*.* %runtimeDir%\Language\*.* /E > nul
+GOTO:EOF
+
+:clean
+    ECHO Cleaning Qvc_Runtime directory...
+    DEL /S /Q %runtimeDir%\*.* > nul
+GOTO:EOF
+
+:doc
+    ECHO Building documentation...
+    %qveDir%\%qveExec% /NoSecurity /r %docDir%\QvcDocumentation.qvw
+GOTO:EOF
+
+:End
 @ECHO ON
